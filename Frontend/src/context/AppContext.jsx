f84b87c966bf275ad8e9caf9929311ee0b06b08a
gourabofficial <gangulygourab3@gmail.com>
2025-04-05 02:10:40 +0530

    home page design , add the banner only

---
+++ Frontend/src/context/AppContext.jsx
@@ -0,0 +1,26 @@
+import { createContext,useContext, useState } from "react";
+import App from "../App";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setisSeller] = useState(null);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+
+  const value = {navigate,user, setUser,isSeller, setisSeller,showUserLogin, setShowUserLogin};
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useAppContect = () => {
+  return useContext(AppContext);
+}
